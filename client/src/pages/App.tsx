a7c1e2465089960c4ad64b2bfeb7e561e6c6cb3c
Diego Giuliani <giuliani.diego@gmail.com>
2021-03-26 22:33:32 -0300

    refactor: Move game state to reducer

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,35 +1,56 @@
-import React, { useState } from 'react';
-import { BoardContainer, Board, StartScreen, ResetScreen } from '../components';
+import React, { useReducer } from 'react';
+import { Board, BoardContainer, ResetScreen, StartScreen } from '../components';
 import { Winner } from '../components/Board';
 import { Header } from '../components/Header';
 
-type GameState = 'start' | 'game' | 'reset';
+interface GameState {
+  state: 'start' | 'game' | 'reset';
+  winner: Winner | null;
+}
+interface GameAction {
+  type: 'GAME_START' | 'GAME_RESET' | 'GAME_END';
+  payload?: Winner | null;
+}
+
+const gameReducer = (state: GameState, action: GameAction): GameState => {
+  switch (action.type) {
+    case 'GAME_START':
+      return { state: 'game', winner: null };
+    case 'GAME_RESET':
+      return { state: 'start', winner: null };
+    case 'GAME_END':
+      return { state: 'reset', winner: action.payload! };
+    default:
+      return state;
+  }
+};
 
 export default function App() {
-  const [gameState, setGameState] = useState<GameState>('start');
-  const [winner, setWinner] = useState<Winner>('x');
+  const [gameState, dispatch] = useReducer(gameReducer, {
+    state: 'start',
+    winner: null,
+  });
   const handleStart = () => {
-    setGameState('game');
-    setWinner(null);
+    dispatch({ type: 'GAME_START' });
   };
   const handleReset = () => {
-    setWinner(null);
-    setGameState('game');
+    dispatch({ type: 'GAME_RESET' });
   };
   const handleGameEnd = (winner: Winner) => {
-    setWinner(winner);
-    setGameState('reset');
+    dispatch({ type: 'GAME_END', payload: winner });
   };
   return (
     <div className="App">
       <Header>Tic-Tac-Toe</Header>
 
-      <BoardContainer animate={gameState}>
-        {gameState === 'start' && <StartScreen onStart={handleStart} />}
+      <BoardContainer animate={gameState.state}>
+        {gameState.state === 'start' && <StartScreen onStart={handleStart} />}
 
-        {gameState === 'game' && <Board onGameEnd={handleGameEnd}></Board>}
-        {gameState === 'reset' && (
-          <ResetScreen winner={winner} onReset={handleReset} />
+        {gameState.state === 'game' && (
+          <Board onGameEnd={handleGameEnd}></Board>
+        )}
+        {gameState.state === 'reset' && (
+          <ResetScreen winner={gameState.winner} onReset={handleReset} />
         )}
       </BoardContainer>
     </div>
